a514cb3967989665d551d518ff9090db8f87d90d
BABABOOSHIKA <89411645+BABABOOSHIKA@users.noreply.github.com>
2021-11-30 21:56:29 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+function startClassification()
+{
+  navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/qySer-5Iv/model.json', modelReady);
+}
+
+function modelReady(){
+  classifier.classify( gotResults);
+}
+
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    random_number_r = Math.floor(Math.random() * 255) + 1;
+    random_number_g = Math.floor(Math.random() * 255) + 1;
+    random_number_b = Math.floor(Math.random() * 255) + 1;
+
+    document.getElementById("result_label").innerHTML = 'I can hear - '+ results[0].label;
+    document.getElementById("result_confidence").innerHTML = 'Accuracy - '+ (results[0].confidence*100).toFixed(2)+" %";
+    document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+    document.getElementById("result_confidence").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+
+    img = document.getElementById('lol dog.png') 
+    img1 = document.getElementById('cat meowing.png')
+    img2 = document.getElementById('hearing.png')
+
+    if (results[0].label == "Bark") {
+      img.src = 'lol dog.png';
+    } else if (results[0].label == "Cat") {
+      img.src = 'cat meowing.png';
+    } else if (results[0].label == "Hearing") {
+      img.src = 'hearing.png';
+    }else{
+      img.src = 'lol dog.png';
+      img1.src = 'cat meowing.png';
+      img2.src = 'hearing.png';
+    }
+  }
+}
